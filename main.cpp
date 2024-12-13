4d7e338e87331776a5493db11490be4c9807a88a
AleRizzi <alex.rizzi2002@gmail.com>
2024-12-13 13:05:33 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,17 +1,1 @@
-int main() {
-    AlberoBinario<int> albero;
 
-    albero.insRadice(11);
-    albero.insFiglioSinistro(0, 4);
-    albero.insFiglioDestro(0, 12);
-    
-    std::cout << "Albero:\n";
-    albero.stampa();
-    
-    albero.rimuovi(2);
-    
-    std::cout << "Albero dopo la rimozione:\n";
-    albero.stampa();
-    
-    return 0;
-}
